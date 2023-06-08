0254b5703e7aa3bbdd31031fdcfef47bb27dabe3
manojkumar1995 <manoj251995kumar@gmail.com>
2023-06-08 22:56:03 +0530

    javascript varianles var,let,const

---
+++ bootstrap5/js/variables.js
@@ -0,0 +1,34 @@
+var fname = 'John';
+let age = 21;
+const salary = 145.8;
+var active = true;
+console.log("salary",salary)
+
+
+// new developer changes
+// name = "Jack";
+// console.log(name)
+
+if(fname == 'John'){
+    let oldage = age;
+    var newage = 24;
+    newage = newage+1;
+    
+    // console.log("John old age is", oldage)
+    // console.log("John new age is",newage)
+    const salary = 234.8;
+    console.log("salary", salary)   
+    
+
+}
+
+
+// console.log("John age is",age)
+// console.log("John new age is",newage)
+
+
+
+// let ,const
+
+
+
