2b456e0c7c14c1801070b5dfdc95029158618fd0
manojkumar1995 <manoj251995kumar@gmail.com>
2023-06-16 23:47:29 +0530

    JS array method part-2

--- bootstrap5/js/arrays.js
+++ bootstrap5/js/arrays.js
@@ -24,6 +24,7 @@ console.log(fruits.slice(0,2)) // slice returns new array with slice version
 console.log("Removes last element in array:", fruits.pop()) // it will directly change original array
 console.log("Add new element in the last position :", fruits.push("strawberry","cherry")) // it will directly change original array
 console.log(fruits.concat(fruits1).concat(fruits2)) // // it will not directly change original array instead of it will a new array
+console.log(fruits.concat(fruits1,fruits2))// it will not directly change original array instead of it will a new array
 // console.log([...fruits,...fruits1,...fruits2]) // using spread operator
 console.log("fruits array end",fruits)
 
