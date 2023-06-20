01243edfe1e773663249c8b522ddff4a93067c3f
manojkumar1995 <manoj251995kumar@gmail.com>
2023-06-20 23:19:35 +0530

    functions parameters default arguments

--- bootstrap5/js/functions.js
+++ bootstrap5/js/functions.js
@@ -25,9 +25,9 @@ const addNew = (a,b)=>{return a+b;}
 
 // const addnew = (a,b)=>{ return a+b }
 
-console.log(add(5,4),sub(5,4), multiple(5,4),divide(5,4))
-console.log(add(3,2),sub(3,2), multiple(3,2),divide(3,2))
-console.log(addNew(9,9))
+// console.log(add(5,4),sub(5,4), multiple(5,4),divide(5,4))
+// console.log(add(3,2),sub(3,2), multiple(3,2),divide(3,2))
+// console.log(addNew(9,9))
 
 // 5 => 5*5 = 25 
 // 6 => 6*6 = 36 
@@ -37,16 +37,61 @@ function square(a){
 
 const square1 = a=>a*a
 
-console.log("square : ",square(35))
-console.log("square1 : ",square1(45))
+// console.log("square : ",square(35))
+// console.log("square1 : ",square1(45))
 
 
 for(let i=1;i<=10;i++){
-    console.log(`2 X ${i} = ${i*2}`)  // `` here `` are template string we can dynamic variables using ${}
+    // console.log(`2 X ${i} = ${i*2}`)  // `` here `` are template string we can dynamic variables using ${}
+}
+
+function greet(message="How are you?",age=10){
+    // console.log("Hi, "+message);
+
+    // console.log("My age is, "+age);
 }
 
 
+greet("Good Morning!")
+greet("Good Afternoon!")
+
+// Function creation using Function Constructor
+const sum = new Function("a","b","return a+b")
+
+let result = sum(5,5)
+// console.log(result)
 
 
+function firstName(fname){
+    return fname
+}
+
+function lastName(lname){
+    return lname
+}
+
+let output1 = firstName("John");
+console.log(output1)
+
+let output2 = lastName("Doe");
+console.log(output2)
+
+// console.log(output1,output2)  // output : John Doe
+
+function fullName(firstName,lastName){
+    console.log(firstName,lastName)
+    // console.log(firstName("Manoj"),lastName("Kumar"))
+    return firstName("Manoj")+" "+lastName("Kumar")
+}
+
+const newfullName = fullName(firstName,lastName);
+console.log(newfullName)
+
+// fullName(function one(fname){ return fname },function two(lname){return lname})
 
 
+const objects = {
+    "key1":"value1",
+    "key2":"value3",
+    "key3":"value3",
+}
