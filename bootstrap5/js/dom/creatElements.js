34cdfa8be6c73ed3ebd439a0129987f0ab4d9fa5
obul63 <obulreddyg27@gmail.com>
2023-07-14 20:30:48 +0530

    tried a task using for in

--- bootstrap5/js/dom/creatElements.js
+++ bootstrap5/js/dom/creatElements.js
@@ -3,14 +3,22 @@ const paragraps = {
     'two':'Second Paragraph',
 }
 
-for (const key in paragraps) {
-    if (Object.hasOwnProperty.call(paragraps, key)) {
-        const element = paragraps[key];
-        console.log(element)
-        var para = document.createElement('p')  // empty para created
-        var node = document.createTextNode(element)
-        para.appendChild(node) // appending textnode to empty paragraph
-        let newElement = document.getElementById("second")
-        newElement.appendChild(para)
-    }
+// for (const key in paragraps) {
+//     if (Object.hasOwnProperty.call(paragraps, key)) {
+//         const element = paragraps[key];
+//         console.log(element)
+//         var para = document.createElement('p')  // empty para created
+//         var node = document.createTextNode(element)
+//         para.appendChild(node) // appending textnode to empty paragraph
+//         let newElement = document.getElementById("second")
+//         newElement.appendChild(para)
+//     }
+// }
+
+const inputField = document.getElementById('inputForm');
+for (const key in paragraps){
+    console.log(key);
+    inputField.value += key + " ";
+    inputField.setAttribute('placeholder', inputField.getAttribute('placeholder') + paragraps[key] + " ");
+
 }
