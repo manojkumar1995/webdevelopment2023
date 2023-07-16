2f24ec62b07fae0894909383ace1af501da6e240
manojkumar1995 <manoj251995kumar@gmail.com>
2023-07-16 23:09:29 +0530

    dynamic inputs and sets in Js

---
+++ bootstrap5/js/concepts/sets.js
@@ -0,0 +1,48 @@
+/*
+const arr = [1,2,3]
+const obj = {"name":"one","symobol":"1"}
+console.log(typeof arr,typeof obj)  // o/p : object object
+console.log( arr, obj) // o/p :  [1, 2, 3] ,{name: 'one', symobol: '1'}
+*/
+
+// finding object length:
+// const obj = {"name":"one","symobol":"1"}
+// console.log(Object.keys(obj).length)
+
+
+const arr = [1,2,3,4,5,6,3,4,5]
+console.log(arr)
+const numberSet =new Set(arr) // set removes all duplicate elements
+//set doesn't maintain oder of sequence /insertion it will be in random order
+console.log(numberSet)
+// Remove duplicates in array use set and convert set to array by spread
+// console.log([...numberSet])
+
+numberSet.add(7)
+numberSet.add({"fname":'John'})
+numberSet.delete(3)
+// console.log(numberSet.entries())
+numberSet.forEach(element=>console.log(element))
+
+numberSet.clear()
+
+console.log(numberSet,numberSet.size) // o/p: Set(6) {1, 2, 3, 4, 5, …} ,6
+
+let nameWeakSet = new WeakSet() //only works withs objects and add object other datatypes not allowed
+
+let key1 = {id :1}
+let key2 = {id :2}
+
+let name1 = {fname: 'John',lname: 'Doe'}
+let name2 = {fname: 'Jack',lname: 'Smith'}
+let something = {one:'two'}
+
+nameWeakSet.add(key1)
+nameWeakSet.add(key2)
+nameWeakSet.add(name1)
+nameWeakSet.add(name2)
+
+// nameWeakSet.add('5')// o/p: throws error since it is a weakSet it cannot handle other data type expect objs
+nameWeakSet.add(something)
+nameWeakSet.delete(something)
+console.log(nameWeakSet)
--- bootstrap5/js/dom/creatElements.js
+++ bootstrap5/js/dom/creatElements.js
@@ -15,10 +15,53 @@ const paragraps = {
 //     }
 // }
 
-const inputField = document.getElementById('inputForm');
-for (const key in paragraps){
-    console.log(key);
-    inputField.value += key + " ";
-    inputField.setAttribute('placeholder', inputField.getAttribute('placeholder') + paragraps[key] + " ");
+// const inputField = document.getElementById('inputForm');
+// for (const key in paragraps){
+//     console.log(key);
+//     inputField.value += key + " ";
+//     inputField.setAttribute('placeholder', inputField.getAttribute('placeholder') + paragraps[key] + " ");
 
+// }
+
+
+const inputsObj = {
+    'one':'First Input Feild',
+    'two':'Second Input Feild',
+    'three':'Third Input Feild',
+    'four':'Fourth Input Feild'
 }
+console.log(Object.entries(inputsObj))
+
+/*
+
+for(const row of Object.entries(inputsObj)){ //row represents key,value of obj
+    console.log(row[0]) // row[0] for keys ; row[1] for value 
+    //here we are not destructuring the row so use index for key,value
+}
+
+*/
+let input = document.createElement('input');
+input.type="text"
+input.className='main'
+input.placeholder= "hello"
+console.log(input)
+// elementDiv = document.getElementById('second')
+// elementDiv.appendChild(input)
+
+
+// dynamic inputs creation
+
+for(const [key,value] of Object.entries(inputsObj)){
+    let input = document.createElement('input');
+    input.type="text"
+    input.className='main'
+    input.placeholder= value
+    input.id = key
+    console.log(input)
+    elementDiv = document.getElementById('second')
+    elementDiv.appendChild(input)
+}
+
+// add bg-colors for above code with even-odd logic
+
+
